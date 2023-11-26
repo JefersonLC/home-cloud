963ce4fa10281904195c23bf377ad23386831487
JefersonLC <c.jeferson.lc@gmail.com>
2023-11-26 00:05:10 -0500

    update: tect center and margin

--- client/src/components/error.tsx
+++ client/src/components/error.tsx
@@ -8,7 +8,7 @@ export default function Error({ message }: ErrorProps) {
   return (
     <div className='flex-grow flex flex-col items-center justify-center text-slate-500'>
       <h5 className='text-5xl'>404</h5>
-      <p className='text-2xl font-bold mb-4'>{message}</p>
+      <p className='mt-2 text-2xl font-bold mb-4 text-center'>{message}</p>
       <Link
         to='/'
         className='bg-slate-700 rounded-lg p-2 text-slate-50 hover:bg-slate-600'
