caced3ef0cd93d301f96c643ba77e63b0d9dc152
JefersonLC <c.jeferson.lc@gmail.com>
2023-11-26 00:03:42 -0500

    add: new icons

---
+++ client/src/components/icons/close-icon.tsx
@@ -0,0 +1,13 @@
+export default function CloseIcon() {
+  return (
+    <svg width='24' height='24' viewBox='0 0 24 24'>
+      <path
+        fill='currentColor'
+        d='M6.225 4.811a1 1 0 0 0-1.414 1.414L10.586 12L4.81 17.775a1
+        1 0 1 0 1.414 1.414L12 13.414l5.775 5.775a1 1 0 0 0
+        1.414-1.414L13.414 12l5.775-5.775a1 1 0 0 0-1.414-1.414L12
+        10.586L6.225 4.81Z'
+      />
+    </svg>
+  )
+}
